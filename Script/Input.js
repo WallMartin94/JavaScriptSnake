765ff3ec59db049edf4a6853f4d489ba8749fd03
WallMartin94 <57918288+WallMartin94@users.noreply.github.com>
2022-02-08 13:33:20 +0100

    Added more appropriate movement
    Stopped the possiblity to change direction 180 degrees.

--- Script/Input.js
+++ Script/Input.js
@@ -1,18 +1,28 @@
 let input = {x:0 , y:0}
-//Adding listener to detect keystrokes
+let lastInput ={x:0, y:0}
+
+//Adding listener to detect keystrokes and if statments to prevent impossible movement.
 window.addEventListener('keydowm', event =>{
 
 switch (event.key){
 case 'ArrowDown':
+    if(lastInput.y!==0)break
     input={x:0, y:+1}
     break
+    
     case 'ArrowUP':
+    if(lastInput.y!==0)break
+
     input={x:0, y:+-1}
     break
+    
     case 'ArrowRight':
+     if(lastInput.x!==0)break
     input={x:+1, y:0}
     break
+    
     case 'ArrowLeft':
+    if(lastInput.x!==0)break
     input={x:-1, y:0}
     break
     
@@ -27,7 +37,7 @@ case 'ArrowDown':
 
 export function getInput(){
 
-
+lastInput=input
 
 
 return input
